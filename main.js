cf99c7fed62a02b5a24ae24d6099a20943b4dfd4
Walker Milhoan <walker@milhoandesign.com>
2024-05-27 10:40:47 -0600

    Tying my hand on my own

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 
 document.addEventListener('DOMContentLoaded', function(){
 
-    
+    document.getElementById('count-el').innerText = 0;
+    document.getElementById('increment-btn');
+    document.getElementById('save-btn');
+
+    let total = 0;
+    let incrementPerson = total ++; 
+
+    function incrementPerson() {
+
+    }
 })
 
